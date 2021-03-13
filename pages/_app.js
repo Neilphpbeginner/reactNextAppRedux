b6c8340a903cbf3ab7854b4dd7ae3242508fde76
Neil Lemmer <lemmer.neil@gmail.com>
2021-03-13 18:24:41 +0200

    Installing Redux in a Nextjs Application, and configuring prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "../styles/styles.css"
+import React from "react";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/styles.css";
 
-export default function _app({ Component, pageProps} ) {
-    return <Component {...pageProps} />
+export default function _app({ Component, pageProps }) {
+  return;
+  <Provider>
+    <Component {...pageProps} />
+  </Provider>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import styles from"../styles/app.module.css"
+import React from "react";
+import { Button } from "react-bootstrap";
+import styles from "../styles/app.module.css";
+import { connect } from "react-redux";
 
-export default function index() {
-    return (
-        <div className={styles.home}>
-            <h1>Hello</h1>
-            <div className={styles.homeButtons}>
-            <Button>
-                Add
-            </Button>
-            <Button>
-                Subtract
-            </Button>
-            </div>
-        </div>
-    )
-}
+export const index = (props) => {
+  return (
+    <div className={styles.home}>
+      <h1>Hello</h1>
+      <div className={styles.homeButtons}>
+        <Button className="buttonSpacing">Add</Button>
+        <Button className="buttonSpacing">Subtract</Button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(index);
